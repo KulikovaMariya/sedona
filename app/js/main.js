cf52b57a052e719a1fcce4006029f196a1d0e06b
Maria Kulikova <m.kulikova@aeronavigator.ru>
2022-01-26 23:57:42 +0300

    add: header layout for mobile resolution

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,16 @@
-import {testFunction} from './scripts/test';
-testFunction();
+const burger = document.querySelector('.js-burger');
+const shadow = document.querySelector('.js-shadow');
+const nav = document.querySelector('.js-nav');
+
+burger.addEventListener('click', ()=>{
+  burger.classList.toggle('header__burger--active');
+  shadow.classList.toggle('shadow--active');
+  nav.classList.toggle('header__nav--active');
+});
+
+
+// shadow.addEventListener('click', ()=>{
+//   burger.classList.remove('header__burger--active');
+//   shadow.classList.remove('shadow--active');
+//   nav.classList.remove('header__nav--active');
+// });
